43d5cf82ecec83c03f1134e3c20e101a55bd3dd6
Abraham Ordoñez <aaob_alexis@hotmail.com>
2022-12-11 02:05:35 -0700

    Refactor  CartList

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,51 +1,56 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { Container, Divider, Grid, Stack, Typography } from "@mui/material";
 import { NavigationContext } from "@react-navigation/native";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { LineCard } from "../LineCard";
 import { getCart, removeLineCart } from "./queries";
 import emptycart from "../../assets/emptycart.png";
 
 export function CartList(props: any) {
-  const { data: userCart, error: errorCart, loading } = useQuery(getCart);
-  const lines = !loading ? userCart.getCart.cartLines : [];
+  const { data, error, loading } = useQuery(getCart);
+
+  const [lines, setLines] = useState([]);
+
+  useEffect(() => {
+    if (!loading) {
+      setLines(data.getCart.cartLines);
+    }
+  }, [data, loading]);
+  
   const [removeCartLine] = useMutation(removeLineCart);
-  console.log("LINESLIST->", lines);
   const navigation = useContext(NavigationContext);
   const handleOnRemoveCartLine = (lineId: number) => {
-    console.log("LINEIDREMOVE->", lineId);
-    try {
-      removeCartLine({
-        variables: {
-          id: userCart.getCart.id,
-          lastUpdate: userCart.getCart.lastUpdate,
-          saleLineId: lineId,
-        },
-      });
-      navigation?.navigate("ShoppingCart", { undefined });
-    } catch (e) {}
+    console.log("🚀 ~ file: index.tsx:24 ~ handleOnRemoveCartLine ~ lineId", lineId)
+    
+    // try {
+    //   removeCartLine({
+    //     variables: {
+    //       id: userCart.getCart.id,
+    //       lastUpdate: userCart.getCart.lastUpdate,
+    //       saleLineId: lineId,
+    //     },
+    //   });
+    //   navigation?.navigate("ShoppingCart", { undefined });
+    // } catch (e) {}
   };
+
+  if (loading) return <Typography variant="h4" textAlign="center">Loading....</Typography>;
+  if (error) return <Typography variant="h4" textAlign="center">Error</Typography>;
+  if (!lines.length) return (
+    <Typography variant="h4" textAlign="center">
+      Sorry, your cart is empty
+    </Typography>
+  )
+
   return (
     <Container maxWidth="xl">
-      {lines.length==0&&(<Typography variant="h4" textAlign="center">Tu carrito está vacío</Typography>)}
-      <Stack spacing={4}>
-        {lines.length == 0 && !loading && (
-          <Stack>
-            <Typography variant="h4" textAlign="center">
-              Sorry, your cart is empty
-            </Typography>
-            <img src={emptycart} />
-          </Stack>
-        )}
-        {lines?.map((line: any) => (
-          <Grid item xs={4}>
-            <LineCard line={line} onRemoveLine={handleOnRemoveCartLine} />
-          </Grid>
-        ))}
-        {lines?.length>0&&{
-          
-        }}
-      </Stack>
+        <Stack spacing={4}>
+          {lines.map((line: any) =>
+            <Grid item xs={4}>
+              <LineCard line={line} onRemoveLine={handleOnRemoveCartLine} />
+            </Grid>
+          )}
+        </Stack>
     </Container>
   );
 }
